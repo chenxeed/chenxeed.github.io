a7adbb1156257609c319acbea9c8c8758853e196
Albert ChenX <chenx.phyt@gmail.com>
2016-10-17 19:00:46 +0700

    (feature) albertliana: add gallery feature

--- albertliana/src/js/main.js
+++ albertliana/src/js/main.js
@@ -1,17 +1,31 @@
-// initialize masonry for gallery
-var msnry = new Masonry( '.gallery-list', {
-  // options
-  itemSelector: '.gallery-list > div',
-  gutter: 10
-});
+$(document).ready( function(e){
 
-// scroll into tab
-$('.tab-title').click(function(e){
-  setTimeout(function(){
-    $(window).scrollTo(e.currentTarget, 500);
-  }, 600);
-});
+  // scroll into tab
+  $('.tab-title').click(function(e){
+    setTimeout(function(){
+      $(window).scrollTo(e.currentTarget, 500);
+    }, 600);
+  });
 
-// initiate modal
-$('#openMapMatrimony').animatedModal();
-$('#openMapReception').animatedModal();
+  // initiate modal
+  $('#openMapMatrimony').animatedModal();
+  $('#openMapReception').animatedModal();
+
+  // viewer
+  new Viewer( $('.gallery-list')[0], {
+    url: function(img){
+      return img.src.replace('_tn', '');
+    }
+  });
+  $('.gallery-list').click(function(){
+
+    var viewer = new Viewer(this, {
+      url: function(img){
+        return img.src.replace('_tn', '');
+      }
+    });
+
+  });
+
+
+});
