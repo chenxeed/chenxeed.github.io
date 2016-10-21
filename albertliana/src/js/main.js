e30e4fe57b045b56eba6386f84b66de85f5a996b
Albert ChenX <chenx.phyt@gmail.com>
2016-10-21 19:46:18 +0700

    (albertliana) integrate invitations with googlesheet live

--- albertliana/src/js/main.js
+++ albertliana/src/js/main.js
@@ -1,44 +1,53 @@
 $(document).ready( function(e){
 
   // validate if visitor have correct ID
-  var shorten = getShortenFromURL( window.location.href );
-  var invitee = getInviteeFromShorten(shorten);
-  if(!invitee){
-    $('#visitorInfoWrapper').show();
-    $('#openVisitorInfo').animatedModal();
-    $('#openVisitorInfo').click();
-    return;
-  } else {
-    $('#visitorInfoWrapper').remove();
-    $('#inviteeName').text(invitee.name);
-  }
+  Invitation(function(invitation){
 
-  // scroll into tab
-  $('.tab-title').click(function(e){
-    setTimeout(function(){
-      $(window).scrollTo(e.currentTarget, 500);
-    }, 600);
-  });
-
-  // initiate modal
-  $('#openMapMatrimony').animatedModal();
-  $('#openMapReception').animatedModal();
-
-  // viewer
-  new Viewer( $('.gallery-list')[0], {
-    url: function(img){
-      return img.src.replace('_tn', '');
+    var shorten = invitation.getShortenFromURL( window.location.href );
+    var invitee = invitation.getInviteeFromShorten(shorten);
+    if(!invitee){
+      $('#visitorInfoWrapper').show();
+      $('#openVisitorInfo').animatedModal();
+      $('#openVisitorInfo').click();
+      return;
+    } else {
+      $('#visitorInfoWrapper').remove();
+      $('#inviteeName').text(invitee.name);
+      initViews();
     }
-  });
-  $('.gallery-list').click(function(){
 
-    var viewer = new Viewer(this, {
+  });
+
+  function initViews(){
+
+    // scroll into tab
+    $('.tab-title').click(function(e){
+      setTimeout(function(){
+        $(window).scrollTo(e.currentTarget, 500);
+      }, 600);
+    });
+
+    // initiate modal
+    $('#openMapMatrimony').animatedModal();
+    $('#openMapReception').animatedModal();
+
+    // viewer
+    new Viewer( $('.gallery-list')[0], {
       url: function(img){
         return img.src.replace('_tn', '');
       }
     });
+    $('.gallery-list').click(function(){
 
-  });
+      var viewer = new Viewer(this, {
+        url: function(img){
+          return img.src.replace('_tn', '');
+        }
+      });
+
+    });
+
+  }
 
 
 });
