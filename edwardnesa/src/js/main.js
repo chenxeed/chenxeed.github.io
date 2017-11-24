86d98786581d370f26b7da07ed7928d2a6d0d4eb
Albert ChenX <chenxeed@gmail.com>
2017-11-25 01:08:26 +0800

    [edwardnesa](new) initial upload - copied originally from denny kristle

---
+++ edwardnesa/src/js/main.js
@@ -0,0 +1,75 @@
+$(document).ready( function(e){
+
+  // validate if visitor have correct ID
+  Invitation(function(invitation){
+
+
+    var password = invitation.getPasswordFromURL( window.location.href );
+    var invitee = invitation.getInviteeFromPassword(password);
+    if(!invitee){
+      $('#visitorInfoWrapper').show();
+      $('#openVisitorInfo').animatedModal();
+      $('#openVisitorInfo').click();
+      return;
+    } else {
+      $('#visitorInfoWrapper').remove();
+      $('#inviteeName').text(invitee.name);
+      initViews();
+    }
+
+    setTimeout(function(){
+      $('#loadingWrapper').fadeOut(500);
+    }, 100);
+  });
+
+  function initViews(){
+
+    // scroll into tab
+    $('.tab-title').click(function(e){
+      setTimeout(function(){
+        $(window).scrollTo(e.currentTarget, 500);
+      }, 600);
+    });
+
+    // initiate modal
+    $('#openMapMatrimony').animatedModal({color: 'rgb(78, 78, 78)'});
+    $('#openMapReception').animatedModal({color: 'rgb(78, 78, 78)'});
+
+    // initiate invitation viewer
+    new Viewer( $('.invitation-list')[0], {
+      url: function(img){
+        return img.src;
+      }
+    });
+    $('.invitation-list').click(function(){
+      var viewer = new Viewer(this, {
+        url: function(img){
+          return img.src;
+        }
+      });
+    });
+
+    // initiate masonry on gallery
+    $('.gallery-list').masonry({
+      columnWidth: '.gallery-list .grid-sizer',
+      gutter: '.gallery-list .gutter-sizer',
+      percentPosition: true
+    });
+
+    // initiate gallery viewer
+    new Viewer( $('.gallery-list')[0], {
+      url: function(img){
+        return img.src;
+      }
+    });
+    $('.gallery-list').click(function(){
+      var viewer = new Viewer(this, {
+        url: function(img){
+          return img.src;
+        }
+      });
+    });
+  }
+
+
+});
