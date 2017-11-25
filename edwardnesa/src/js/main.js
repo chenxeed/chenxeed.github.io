541e02ffe88b68ea8170751aaca3cbec730d38cd
Albert ChenX <chenxeed@gmail.com>
2017-11-25 14:45:55 +0800

    [edwardnesa](rev) remove invitee validation

--- edwardnesa/src/js/main.js
+++ edwardnesa/src/js/main.js
@@ -1,26 +1,6 @@
 $(document).ready( function(e){
 
-  // validate if visitor have correct ID
-  Invitation(function(invitation){
-
-
-    var password = invitation.getPasswordFromURL( window.location.href );
-    var invitee = invitation.getInviteeFromPassword(password);
-    if(!invitee){
-      $('#visitorInfoWrapper').show();
-      $('#openVisitorInfo').animatedModal();
-      $('#openVisitorInfo').click();
-      return;
-    } else {
-      $('#visitorInfoWrapper').remove();
-      $('#inviteeName').text(invitee.name);
-      initViews();
-    }
-
-    setTimeout(function(){
-      $('#loadingWrapper').fadeOut(500);
-    }, 100);
-  });
+  initViews();
 
   function initViews(){
 
