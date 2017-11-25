e9542fdbae9d869dce470a67c899eb21a028a8a4
Albert ChenX <chenxeed@gmail.com>
2017-11-25 16:54:04 +0800

    [edwardnesa](rev) add feature - add music and toggle to play music - add timer

--- edwardnesa/src/js/main.js
+++ edwardnesa/src/js/main.js
@@ -4,6 +4,40 @@ $(document).ready( function(e){
 
   function initViews(){
 
+    // play the music
+    var sound = new Howl({
+      src: ['src/audio/perfect.mp3', 'src/audio/perfect.ogg'],
+      autoplay: true,
+      loop: true,
+      volume: 0.5
+    });
+    $('.music.music-play').hide();
+    $('.music.music-pause').on('click', function(){
+      sound.fade(1, 0, 1000);
+      $('.music.music-pause').hide();
+      $('.music.music-play').show();
+    });
+    $('.music.music-play').on('click', function(){
+      sound.fade(0, 1, 1000);
+      $('.music.music-play').hide();
+      $('.music.music-pause').show();
+    });
+
+    // init flipclock
+    // Grab the current date
+    var currentDate = new Date();
+    // Set some date in the future. In this case, it's always Jan 1
+    var futureDate  = new Date(2017, 11, 9);
+    // Calculate the difference in seconds between the future and current date
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+    // Only show countdown when the countdown hasn't reach 0
+    if( diff > 0 ) {
+      $('.countdown').FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        countdown : true
+      });
+    }
+
     // scroll into tab
     $('.tab-title').click(function(e){
       setTimeout(function(){
