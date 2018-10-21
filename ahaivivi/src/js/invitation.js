6d0aae5dbaac8959f1c7d3b98552eebe691b670a
Albert ChenX <chenxeed@gmail.com>
2018-10-21 22:58:44 +0800

    [ahaivivi](init) initialization

---
+++ ahaivivi/src/js/invitation.js
@@ -0,0 +1,41 @@
+function Invitation(cb){
+
+  $.ajax('https://spreadsheets.google.com/feeds/list/14SvDtFFYpCw4QbEeAcRj0T2gWR6tTisM9c84VnWfBf0/1/public/values?alt=json')
+  .done(function(output){
+    var invitation_names = output.feed.entry.map(function(row){
+      var to_return = {};
+      for(var key in row){
+        to_return[key.replace('gsx$','')] = row[key].$t;
+      }
+      return to_return;
+    });
+
+    function getInviteeFromPassword(password){
+      return invitation_names.filter(function(invitee){
+        return invitee.password === password;
+      })[0];
+    }
+
+    function getList(){
+      return invitation_names;
+    }
+
+    cb({
+      getInviteeFromPassword : getInviteeFromPassword,
+      getPasswordFromURL : getPasswordFromURL,
+      getURLFromPassword : getURLFromPassword,
+      getList : getList
+    });
+
+  });
+
+  // the url format is chenxeed.github.io/albertliana/index.html?o=password
+  function getPasswordFromURL(url){
+    return url.split('index.html?o=')[1];
+  }
+
+  function getURLFromPassword(password){
+    return window.location.origin+'/ahaivivi/index.html?o='+password;
+  }
+
+}
---
+++ ahaivivi/src/js/main.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  initViews();
+  $('#loadingWrapper').fadeOut(500);
+
+  function initViews(){
+
+    // play the music
+    // var sound = new Howl({
+    //   src: ['src/audio/perfect.mp3', 'src/audio/perfect.ogg'],
+    //   autoplay: true,
+    //   loop: true,
+    //   volume: 0.5
+    // });
+    // $('.music.music-play').hide();
+    // $('.music.music-pause').on('click', function(){
+    //   sound.pause();
+    //   $('.music.music-pause').hide();
+    //   $('.music.music-play').show();
+    // });
+    // $('.music.music-play').on('click', function(){
+    //   sound.play();
+    //   $('.music.music-play').hide();
+    //   $('.music.music-pause').show();
+    // });
+
+    // // init flipclock
+    // // Grab the current date
+    // var currentDate = new Date();
+    // // Set some date in the future. In this case, it's always Jan 1
+    // var futureDate  = new Date(2017, 11, 9);
+    // // Calculate the difference in seconds between the future and current date
+    // var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+    // // Only show countdown when the countdown hasn't reach 0
+    // if( diff > 0 ) {
+    //   $('.countdown').FlipClock(diff, {
+    //     clockFace: 'DailyCounter',
+    //     countdown : true
+    //   });
+    // }
+
+    // scroll into tab
+    $('.tab-title').click(function(e){
+      setTimeout(function(){
+        $(window).scrollTo(e.currentTarget, 500);
+      }, 600);
+    });
+
+    // initiate modal
+    $('#openMapReception').animatedModal({color: 'rgb(78, 78, 78)'});
+
+    // initiate invitation viewer
+    new Viewer( $('.invitation-list')[0], {
+      url: function(img){
+        return img.src;
+      }
+    });
+    $('.invitation-list').click(function(){
+      var viewer = new Viewer(this, {
+        url: function(img){
+          return img.src;
+        }
+      });
+    });
+
+    // initiate masonry on gallery
+    var $gallery = $('.gallery-list').masonry({
+      columnWidth: '.gallery-list .grid-sizer',
+      gutter: '.gallery-list .gutter-sizer',
+      percentPosition: true
+    });
+    $('#toggle-gallery').on('click', function(){
+      $gallery.masonry('layout');
+    })
+
+    // initiate gallery viewer
+    new Viewer( $('.gallery-list')[0], {
+      url: function(img){
+        return img.src;
+      }
+    });
+    $('.gallery-list').click(function(){
+      var viewer = new Viewer(this, {
+        url: function(img){
+          return img.src;
+        }
+      });
+    });
+  }
+});
